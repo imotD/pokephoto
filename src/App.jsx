aec15cdc4aab3298ff9ac569368264fe489b0ec1
Tommy Alhamra <tommy.alhamra@ist.id>
2024-11-20 22:52:07 +0700

    fix: pagination and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Loading from "./components/Loading";
-import Detail from "./components/Detail";
+import MiniDetail from "./components/MiniDetail";
 import Searching from "./components/Searching";
 
 import Header from "./components/molecules/Header";
 import Footer from "./components/molecules/Footer";
-
+import ListPokemon from "./components/molecules/ListPokemon";
 import Button from "./components/atoms/Button";
 
 function App() {
   const pokeApiUrl = useSelector((state) => state.global.pokeApiUrl);
   const pokeSpritesUrl = useSelector((state) => state.global.pokeSpritesUrl);
+  const pokeApiUrlList = useSelector((state) => state.global.pokeApiUrlList);
 
   const [dataList, setDataList] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(false);
   const [loading, setLoading] = useState(false);
   const [loadingDetail, setLoadingDetail] = useState(false);
   const [detail, setDetail] = useState({});
+
+  const [urlListPoke, setUrlListPoke] = useState(pokeApiUrlList);
 
   useEffect(() => {
     getAllPokemon();
@@ -27,11 +31,19 @@ function App() {
 
   const getAllPokemon = () => {
     setLoading(true);
+
     axios
-      .get(pokeApiUrl)
+      .get(urlListPoke)
       .then((res) => {
-        const responseDataList = res.data.results;
-        const dataAddNumber = responseDataList.map((res) => {
+        const responseDataList = res.data;
+
+        if (responseDataList.next === null) {
+          setIsDisabled(true);
+        } else {
+          setIsDisabled(false);
+        }
+
+        const dataAddNumber = responseDataList.results.map((res) => {
           const numberImage = res.url.split("/");
 
           return {
@@ -40,14 +52,15 @@ function App() {
           };
         });
 
-        setDataList(dataAddNumber);
+        setUrlListPoke(responseDataList.next);
+
+        setDataList((prevData) => [...prevData, ...dataAddNumber]);
       })
       .catch((e) => {
         console.log("🚀 ~ error", e);
       })
       .finally(() => {
         setLoading(false);
-        setDetail({});
       });
   };
 
@@ -86,65 +99,72 @@ function App() {
       .get(pokeApiUrl + id)
       .then((res) => {
         setDetail(res.data);
-        setLoadingDetail(false);
       })
       .catch((e) => {
+        console.error(e);
+      })
+      .finally(() => {
         setLoadingDetail(false);
       });
   };
 
-  const listItems = dataList.map((value) => (
-    <div key={value.id} onClick={() => onClickDetail(value.id)}>
-      <img
-        src={`${pokeSpritesUrl}${value.id}.gif`}
-        className="h-10 w-10 border-2 border-black rounded-full bg-slate-200 my-1 hover:bg-yellow-300 cursor-pointer"
-        alt="img"
-        title={value.name}
-        loading="lazy"
-      />
-    </div>
-  ));
+  const listItems = (
+    <>
+      <div className="mt-10 grid grid-cols-6 align-center m-auto justify-items-center ">
+        {dataList.map((value) => (
+          <div key={value.id} onClick={() => onClickDetail(value.id)}>
+            <img
+              src={`${pokeSpritesUrl}${value.id}.gif`}
+              className="h-10 w-10 border-2 border-black rounded-full bg-slate-200 my-1 hover:bg-yellow-300 cursor-pointer"
+              alt="img"
+              title={value.name}
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex align-center justify-center py-5 mb-5">
+        <Button
+          onClick={getAllPokemon}
+          className={
+            ("ml-2", isDisabled ? "bg-yellow-100 cursor-not-allowed" : "")
+          }
+          type="button"
+          isDisabled={isDisabled}
+        >
+          Show more
+        </Button>
+      </div>
+    </>
+  );
 
   return (
-    <div className="App">
-      {/* <div className="min-h-28 absolute pattern-box top-0 bottom-0 right-0 left-0"></div> */}
+    <div className="App h-screen">
+      {/* HEADER */}
       <Header />
+
+      {/* CONTENT */}
       <div className="flex items-center gap-2 justify-center">
         <div className="pattern-box"></div>
-        <div className="w-1/2 p-5 m-auto">
+        <div className="w-1/2 p-5 m-auto ">
           {/* SEARCHING */}
           <Searching onHandleSubmit={onClickSearch} />
 
           {/* CANVAS POKEMON */}
-          <div className="mt-10 grid grid-rows-4 grid-flow-col items-center gap-2 justify-items-center w-1/3 m-auto">
-            {loading ? <Loading /> : dataList.length ? listItems : ""}
-          </div>
-
-          {/* PAGINATION */}
-          <div className="flex flex-col items-center mt-5">
-            <span className="text-sm text-gray-700">
-              Showing <span className="font-semibold text-gray-900 ">1</span> to{" "}
-              <span className="font-semibold text-gray-900 ">10</span> of{" "}
-              <span className="font-semibold text-gray-900 ">100</span> Entries
-            </span>
-
-            <div className="inline-flex mt-2 xs:mt-0">
-              <Button className="ml-2" type="submit">
-                Prev
-              </Button>
-              <Button className="ml-2" type="submit">
-                Next
-              </Button>
+          <div className="w-80 h-96 overflow-scroll	m-auto mt-6 border-2 border-black gradient-list-poke">
+            <div className="">
+              {loading ? <Loading /> : dataList.length ? listItems : ""}
             </div>
           </div>
         </div>
 
         {/* PAGE DETAILS */}
         <div className="w-1/2 p-5">
-          <Detail loading={loadingDetail} data={detail} />
+          <MiniDetail loading={loadingDetail} data={detail} />
         </div>
       </div>
 
+      {/* FOOTER */}
       <Footer year="2023" />
     </div>
   );
