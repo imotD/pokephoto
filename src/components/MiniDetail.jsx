875c54d810211b0fb277220c1e4d7f6cb2a1f847
Tommy Alhamra <45749487+imotD@users.noreply.github.com>
2024-12-05 13:24:30 +0700

    5 responsife web (#12)
    * fix(image): change url path image
    
    * fix: responsive design
    
    ---------
    
    Co-authored-by: Tommy Alhamra <tommy.alhamra@ist.id>

--- src/components/MiniDetail.jsx
+++ src/components/MiniDetail.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import PropTypes from "prop-types";
-import pokeBallLoading from "../../image/pokeball.gif";
+import pokeBallLoading from "../../src/assets/images/pokeball.gif";
 
 export default function Detail({ data, loading }) {
   const pokeSpritesPhotoUrl = useSelector(
@@ -31,7 +31,7 @@ export default function Detail({ data, loading }) {
       <p className="capitalize text-sm my-2 font-bold">
         {data.name || "PokeBall"}
       </p>
-      <div className="absolute rotate-12 -top-1 -right-11 bg-amber-500/55 w-28 h-8 sticky-tape">
+      <div className="absolute rotate-12 -top-1 sm:-right-11 right-0 bg-amber-500/55 w-28 h-8 sticky-tape">
         <span className="font-bold text-slate-50 drop-shadow-lg">
           #{data?.id || "00"}
         </span>
